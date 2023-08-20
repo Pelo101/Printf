1c5afef9c8b7065cfae52884ea7cca2f512c6a2a
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-20 08:17:06 -0700

    updated

--- main.h
+++ main.h
@@ -1,13 +1,19 @@
-int _putchar(char c);
-int _printf(const char *format, ...);
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stddef.h>   //to check for null
+#include <stddef.h>
 
-#define void(x)
 #define BUFF_SIZE 1024
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
 
 
 /* FLAGS */
@@ -18,23 +24,21 @@ int _printf(const char *format, ...);
 
 
 
-/*
-* @fmt: The format.
-* @fn: The function associated.
-*/
+/**
+ *struct FormatHandler - handles format
+ *@fmt - The format.
+ *@fn: The function associated.
+ */
 
+struct FormatHandler
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-/* function to handle other specifiers */
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
+}
 
 
 
 
 
-#endif
+#endif /* MAIN_H_ */
 
