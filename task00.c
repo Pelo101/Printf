bd9e0d55aee4c02eda8d93566d88f033f733ab31
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-20 09:50:38 -0700

    task 0

---
+++ task00.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - print formatted strings.
+ * @format: format string.
+ * Return: number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+    va_list ptr;
+    int count = 0;
+    int c;
+    char *s;
+
+    va_start(ptr, format);
+    while (*format != '\0')
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                c = va_arg(ptr, int);
+                _putchar(c);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                s = va_arg(ptr, char *);
+                if (s != NULL)
+                {
+                    while (*s != '\0')
+                    {
+                        _putchar(*s);
+                        s++;
+                        count++;
+                    }
+                }
+            }
+            else if (*format == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else
+            {
+                _putchar(*format);
+                count++;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(ptr);
+    return count;
+}
+
