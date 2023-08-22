76ebf743c249418a4e56858909fa211f5f4a38b1
Mal-95 <malebo445.ml@gmail.com>
2023-08-22 15:14:34 -0700

    update

---
+++ print_buff.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+/**
+ *my_function - print buffer.
+ *@fd: file descriptor.
+ */
+
+
+void my_function(int fd)
+{
+	ssize_t bytes_written = write(fd, buffer, strlen(buffer));
+
+	char buffer[1024];
+
+	memset(buffer, 0, sizeof(buffer));
+
+	if (bytes_written == -1)
+	{
+		return ;
+
+	}
+}
---
+++ print_handlers.c
@@ -0,0 +1,15 @@
+#ifndef PRINT_HANDLERS_
+#define PRINT_ HANDLERS_
+
+/*Handler function prototypes*/
+
+
+
+int print_integer(va_list args);
+int print_decimal(va_list args);
+int print_pointer(va_list args);
+int print_rot(va_list args);
+int print_binary(va_list args);
+
+
+#endif
