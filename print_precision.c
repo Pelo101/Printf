d0d1c51c0e915537deee8dd334b38667c7f7f3fd
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-23 03:33:58 -0700

    task 10

--- print_precision.c
+++ print_precision.c
@@ -1,15 +1,61 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-*print_size - Print value with specified precision.
-*@format: format string.
-*@value: print value.
+*print_integer_with_precision - print integer.
+*@value: parameter
+*@precision: parameter
 */
 
+void print_integer_with_precision(int value, int precision)
+{
+	int i;
+	char buffer[20];
+	int length = 0;
+
+	if (value < 0)
+	{
+		buffer[length++] = '-';
+		value = -value;
+	}
+
+	do {
+
+		buffer[length++] = value % 10 + '0';
+		value /= 10;
+	} while (value > 0);
+
+
+	while (length < precision)
+	{
+		buffer[length++] = '0';
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = buffer[i];
+
+		buffer[i] = buffer[length - i - 1];
+		buffer[length - i - 1] = temp;
+	}
+
+	for (i = 0; i < length; i++)
+
+	{
+		_putchar(buffer[i]);
+	}
+	_putchar('\n');
+}
+
+
+/**
+*print_size - print characters with precision
+*@format: parameter.
+*@value: parameter.
+*/
 void print_size(const char *format, int value)
 {
 	int precision = -1;
-
 
 	while (*format)
 	{
@@ -17,9 +63,9 @@ void print_size(const char *format, int value)
 		{
 			precision = 0;
 			++format;
+
 			while (*format >= '0' && *format <= '9')
 			{
-
 				precision = precision * 10 + (*format - '0');
 				++format;
 			}
@@ -28,6 +74,7 @@ void print_size(const char *format, int value)
 		++format;
 	}
 
-      print_integer_with_precision(value, precision);
+	print_integer_with_precision(value, precision);
 }
+
 
